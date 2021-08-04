97ccb5af05e5b63ff3046f9a781cc487583a8d94
Jim Sharples <jamie.sharples@gmail.com>
2021-08-04 21:21:15 +1000

    Add sql scripts for demonstration data

---
+++ sql/department.sql
@@ -0,0 +1,13 @@
+create table department;
+
+INSERT INTO cms.department (id, name) VALUES (1, 'Avengers Tower');
+INSERT INTO cms.department (id, name) VALUES (2, 'Avengers Complex');
+INSERT INTO cms.department (id, name) VALUES (3, 'Shield');
+INSERT INTO cms.department (id, name) VALUES (5, 'name');
+INSERT INTO cms.department (id, name) VALUES (6, 'Test');
+INSERT INTO cms.department (id, name) VALUES (7, 'test2');
+INSERT INTO cms.department (id, name) VALUES (8, 'Test3');
+INSERT INTO cms.department (id, name) VALUES (9, 'Tst4');
+INSERT INTO cms.department (id, name) VALUES (10, 'Test Name');
+INSERT INTO cms.department (id, name) VALUES (11, 'Name');
+INSERT INTO cms.department (id, name) VALUES (12, 'Name');
